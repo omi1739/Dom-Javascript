ce31db4297681d773bd11e65718eca87341f5421
Omi <seyam.islam020@gmail.com>
2026-02-14 22:45:27 +0600

    using dom change the background color

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const hi = document.getElementById('btn-hi').addEventListener('click' , ()=>{
     const sayHi = document.getElementById('hi-omi').innerText='Hi OMIBOSS';
 }) 
+
+const backgroundColor = document.getElementById('background').addEventListener('click', ()=>{
+   document.body.style.background='green';
+})
